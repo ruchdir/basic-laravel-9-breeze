cae531a0207c45acd4ea3a9860186b18ce3d4b78
ruchdir <ruchdir@bfsasia.com>
2022-07-07 12:07:42 +0700

    Add is admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,6 +14,8 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+        \App\Models\User::factory(1)->create([
+            'is_admin' => true,
+        ]);
     }
 }
