a82cf7877bcfbfb3ec95faf22125eb874f0555a1
ruchdir <ruchdir@bfsasia.com>
2022-07-07 14:57:01 +0700

    Add Validate Post by Form Request

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -38,14 +39,9 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        $request->validate([
-            'title' => 'required',
-            'post_text' => 'required',
-            'category_id' => 'required',
-        ]);
-        
+
         Post::create([
             'title' => $request->input('title'),
             'post_text' => $request->input('post_text'),
