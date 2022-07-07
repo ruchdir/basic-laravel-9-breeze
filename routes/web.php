3b378f9c8c742929834dd75f55f9e876167ee328
ruchdir <ruchdir@bfsasia.com>
2022-07-07 14:01:35 +0700

    Update to Route Group

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware'=> ['auth']], function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    Route::resource('categories', CategoryController::class);
+});
+
+
 
 require __DIR__.'/auth.php';
 
-Route::resource('categories', CategoryController::class);
