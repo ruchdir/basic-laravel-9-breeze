dfa4e8fb357a24d3d5b46937cf54deb6b4f57c1c
ruchdir <ruchdir@bfsasia.com>
2022-07-07 12:15:22 +0700

    Add Category Controller

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <a class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out" href="{{ route('categories.index') }}">Categories</a>
                 </div>
             </div>
 
